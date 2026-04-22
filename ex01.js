27f8456d138c1dc454f63a843dde631a3e7d3036
mariavamorim <maria.v.amorim@edu.senai.br>
2026-04-22 16:24:39 -0300

    ex 2,3,4 e 5

--- ex01.js
+++ ex01.js
@@ -1,14 +1,13 @@
 // Importando o módulo que você instalou via terminal
 const prompt = require('prompt-sync')();
 
-console.log("--- Soma de Valores ---");
+console.log("--- Conversor de Temperatura ---");
 
-// Importante: prompt sempre lê TEXTO. 
-// Usamos Number() para converter, igual ao (int) ou (float) no C.
-let num1 = Number(prompt("Digite o primeiro número: "));
-let num2 = Number(prompt("Digite o segundo número: "));
+// Entrada de dados (lembrando: sempre vem como texto, então usamos Number)
+let celsius = Number(prompt("Digite a temperatura em Celsius: "));
 
-let soma = num1 + num2;
+// Processamento (fórmula F = C * 9/5 + 32)
+let fahrenheit = celsius * 9/5 + 32;
 
-// Template String (usa crases): facilita exibir variáveis no meio do texto
-console.log(`O resultado da soma entre ${num1} e ${num2} é: ${soma}`);
+// Saída
+console.log(`A temperatura de ${celsius}°C em Fahrenheit é: ${fahrenheit}°F`);
