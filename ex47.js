16c78b547ff1ac73649c201d4e1c693643b94bd0
Your Name <you@example.com>
2026-04-23 12:12:25 -0300

    Concluído Exercíco 47:Valores Divisíveis por 4 Menores que 200

---
+++ ex47.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+console.log("--- Valores Divisíveis por 4 Menores que 200 ---");
+
+// PARA cada número DE 4 a 196 COM PASSO 4 FAÇA
+for(let numero =4; numero <= 196; numero += 4) {
+console.log(numero); // EXIBIR número
+}
+
