c19afbdadf58906c6b9794c34e0b910f48cff19a
Your Name <you@example.com>
2026-04-23 08:34:04 -0300

    Concluído Exercíco 36:Par ou Ímpar

---
+++ ex36.js
@@ -0,0 +1,13 @@
+const prompt = require('prompt-sync')();
+
+console.log("---Par ou Ímpar---");
+
+// Entrada de dados 
+let numero = Number(prompt("Insira um valor inteiro:"));
+
+// Processamento
+if(numero % 2 == 0) {
+    console.log("par");
+}else {
+    console.log(ímpar);
+}
